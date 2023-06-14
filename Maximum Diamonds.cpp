d062e80fb452fc8a900a45b0a956ad42049b3fcb
Siddh Srivastava <118525926+Siddh-s@users.noreply.github.com>
2023-06-14 20:21:43 +0530

    Create Maximum Diamonds.cpp

---
+++ Maximum Diamonds.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+     long long maxDiamonds(int A[], int N, int K) {
+        // code here
+        long long ans=0;
+        priority_queue<int> pq;
+        for(int i=0;i<N;i++)pq.push(A[i]);
+        while(K--){
+            int t=pq.top();
+            ans+=t;
+            pq.pop();
+            pq.push(t/2);
+        }
+        return ans;
+    }
+};
