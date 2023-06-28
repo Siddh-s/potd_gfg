cc087684aae5f7997db59a3b7fec3af495822674
Siddh Srivastava <118525926+Siddh-s@users.noreply.github.com>
2023-06-28 17:32:45 +0530

    Create Maximum Dpeth of Binary Tree.cpp

---
+++ Maximum Dpeth of Binary Tree.cpp
@@ -0,0 +1,8 @@
+class Solution{
+  public:
+    /*You are required to complete this method*/
+   int maxDepth(Node *root) {
+    if(!root) return 0;
+    return 1+max(maxDepth(root->left),maxDepth(root->right));
+}
+};
