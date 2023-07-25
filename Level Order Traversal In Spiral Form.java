d80b25697b971beeb675f12ce62e731485fab90a
Siddh Srivastava <118525926+Siddh-s@users.noreply.github.com>
2023-07-25 19:56:43 +0530

    Create Level Order Traversal In Spiral Form.java

---
+++ Level Order Traversal In Spiral Form.java
@@ -0,0 +1,22 @@
+   ArrayList<Integer> findSpiral(Node root) 
+    {
+        // Your code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        boolean b = true;
+        while(q.size()!=0){
+            int s = q.size();
+            ArrayList<Integer> level = new ArrayList<>();
+            for(int i=0;i<s;i++){
+                Node temp = q.remove();
+                level.add(temp.data);
+                if(temp.left!=null)q.add(temp.left);
+                if(temp.right!=null)q.add(temp.right);
+            }
+            if(b==true)Collections.reverse(level);
+            for(int i=0;i<s;i++)ans.add(level.get(i));
+            b=!b;
+        }
+        return ans;
+    }
