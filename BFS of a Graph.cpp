c6044fe97c54b398800cceecbb34224a7f916fdc
Siddh Srivastava <118525926+Siddh-s@users.noreply.github.com>
2023-07-31 16:01:45 +0530

    Create BFS of a Graph.cpp

---
+++ BFS of a Graph.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<int>res;
+        queue<int>q;
+        q.push(0);
+        map<int,int>vis;
+        vis[0]=1;
+        while(!q.empty()){
+            int topp=q.front();
+            q.pop();
+            res.push_back(topp);
+            for(auto child:adj[topp]){
+                if(!vis[child]){
+                    vis[child]=1;
+                    q.push(child);
+                }
+            }
+        }
+        return res;
+    }
+};
