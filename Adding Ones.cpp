efd9761d04cc4c7563da9e801cac790a2edbce41
Siddh Srivastava <118525926+Siddh-s@users.noreply.github.com>
2024-08-21 19:45:34 +0530

    Update Adding Ones.cpp

--- Adding Ones.cpp
+++ Adding Ones.cpp
@@ -1,7 +1,7 @@
  void update(int a[], int n, int updates[], int k)
     {
         // Your code goes here
-        for(int i=0;i<k;i++){
+        for(int i=0;i<=k-1;i++){
             a[updates[i]-1]++;
         }
         int temp=0;
